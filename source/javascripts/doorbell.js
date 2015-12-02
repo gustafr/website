bb4e2805faadbdd17260177f9b50fe0c99c4cbcd
Raoul DIFFOUO <diraulo@gmail.com>
2015-12-03 01:28:08 +0200

    translate doorbell form from english to swedish #4

--- source/javascripts/doorbell.js
+++ source/javascripts/doorbell.js
@@ -1,4 +1,23 @@
 window.doorbellOptions = {
+  strings: {
+    'feedback-button-text': 'Ställa en fråga?',
+
+    'title': 'Feed-back',
+    'intro-text': 'Har du en fråga? Feed-back? Vill bara säga hej? Slå oss en signal nedan.',
+    'feedback-textarea-placeholder': 'Skicka oss dina åsikter eller förslag...',
+    // 'feedback-label': '(empty)',
+    'email-input-placeholder': 'Din mailaderss',
+    // 'email-label': '(tom)',
+    'attach-a-screenshot': 'Visa oss vad du tittar på',
+    'submit-button-text': 'Skicka',
+    // 'add-attachments-label': 'Add an attachment',
+
+    'message-success' : 'Meddelandet är skickat!',
+    'message-error-missing-email': 'Hmm... Du måste ange en email adress',
+    'message-error-invalid-email': 'Email adressen du angav verkar ogiltig...',
+    'message-error-missing-message': 'Meddelandet är tomt, var det meningen?',
+    'message-error-message-too-short': 'Lite väl kort meddelande...'
+  },
   appKey: 'zF0anPyXM2iItB1yfkqhwe8iMBdR4mBuiKPuwH2oe0OuFBvK9IFoksmZVigjgaGS'
 };
 (function(d, t) {
