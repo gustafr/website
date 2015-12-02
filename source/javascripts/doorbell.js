a325f246e0f73cfe7cb35ab1f77afa8c50afa70a
Raoul DIFFOUO <diraulo@gmail.com>
2015-12-03 01:11:30 +0200

    dorbell apikey

--- source/javascripts/doorbell.js
+++ source/javascripts/doorbell.js
@@ -1,5 +1,5 @@
 window.doorbellOptions = {
-  appKey: 'f0rsfH4ny8KpwT2h2Dq4upebSP4cjPa0yUhbRuObUwWnQULtH1CsSZeREpCmv47N'
+  appKey: 'zF0anPyXM2iItB1yfkqhwe8iMBdR4mBuiKPuwH2oe0OuFBvK9IFoksmZVigjgaGS'
 };
 (function(d, t) {
   var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/1652?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
