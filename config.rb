6c205942847751a15f538355e00861d59e20d1d6
tochman <thomas@nocebo.se>
2015-12-04 12:48:34 +0100

    updated 'config.rb' with proper uri - fixed typo

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ Dir['lib/*.rb'].each { |file| require file }
 activate :directory_indexes
 activate :meta_tags
 
-set :url_root, 'http://vraftacademy.se'
+set :url_root, 'http://craftacademy.se'
 activate :search_engine_sitemap
 
 # Change Compass configuration
